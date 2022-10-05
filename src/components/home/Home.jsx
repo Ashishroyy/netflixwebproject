6c3b925e9f7509323bd0713c3727a8970915b98e
Ashish  kumar <104258722+Ashishroyy@users.noreply.github.com>
2022-10-05 10:15:01 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -52,7 +52,7 @@ const Home = () => {
       };
 
   const fetpopular = async()=>{
-    const {data: {results}} = await axios.get(`${url}/movie/${popular}?api_key=${apikey}&page=5`)
+    const {data: {results}} = await axios.get(`${url}/movie/${popular}?api_key=${apikey}&page=1`)
     setpopularmovies(results)
       };
 
